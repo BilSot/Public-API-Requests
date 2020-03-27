4cd8e91a9fbe3deb0baece881942ee59e6f79567
Biljana <sotirovskabiljana@gmail.com>
2020-03-27 22:23:39 +0100

    Functions documentation added

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ const nationalities = ['gb', 'us'];
 var userDataArray = [];
 var filteredArray = [];
 
+/**
+ * Once the page loads, a call to the Random Users API is performed
+ * If data are retrieved successfully, the users are displayed on the DOM
+ * A search form is added on the page
+ */
 window.onload = function () {
     document.querySelector('div.gallery').innerHTML =
         `<h2>Loading...</h2>`;
@@ -16,7 +21,7 @@ window.onload = function () {
             document.querySelector('div.gallery').innerHTML = '';
             userDataArray = data.results;
             generateCards(userDataArray);
-            addSearchTextField();
+            createSearchForm();
         })
         .catch(err => {
             document.querySelector('div.gallery').innerHTML = `<h2>Please try again later. Some issue in the network</h2>`;
@@ -24,6 +29,13 @@ window.onload = function () {
         })
 };
 
+/**
+ * Performs the call to the API and calls checkStatus.
+ * If the Promise was resolved, the response is returned in JSON format;
+ * if the Promise was rejected, the error that occurred is printed in the console
+ * @param {String} url
+ * @return {Promise}
+ */
 function fetchData(url) {
     return fetch(url)
         .then(checkStatus)
@@ -31,7 +43,13 @@ function fetchData(url) {
         .catch(err => console.error("Error while fetching data", err))
 }
 
+/**
+ * Checks for the status of the Response. If OK, resolves the Promise, otherwise it rejects it
+ * @param {Response} response
+ * @return {Promise}
+ */
 function checkStatus(response) {
+    console.log(response);
     if (response.ok) {
         return Promise.resolve(response);
     } else {
@@ -39,6 +57,13 @@ function checkStatus(response) {
     }
 }
 
+/**
+ * Creates the container elements (cards) with the users' info and appends them to the `gallery` div element.
+ * Attaches the click event on each of the containers.
+ * Creates a modal pop-up with additional information about the selected user.
+ * Appends two buttons on the modal, previous and next, for toggling between the users
+ * @param {Array} users
+ */
 function generateCards(users) {
     if(users.length == 0){
         document.querySelector('div.gallery').innerHTML = `<h2>No employees found</h2>`;
@@ -69,13 +94,18 @@ function generateCards(users) {
             $(divCard).click(event => {
                 let userDataIndex = event.target.closest('div.card').id;
                 createModal();
-                addBrowsingButtons(userDataIndex);
+                addBrowsingButtons();
+                attachEventsOnModalButtons(userDataIndex);
                 displayUserInfo(userDataIndex);
             });
         }
     }
 }
 
+/**
+ * Creates the modal pop-up and attaches it to the body of the page.
+ * Adds animations to the modal, on open and close action
+ */
 function createModal() {
     let divModalContainer = document.createElement('div');
     divModalContainer.classList.add("modal-container");
@@ -99,6 +129,11 @@ function createModal() {
     });
 }
 
+/**
+ * Retrieves the user's object, on the userDataIndex, in the userDataArray and populates the content of the modal pop-up with the user's info.
+ * Transforms the date of birth to the format dd/mm/yy, using a reg exp
+ * @param {Number} userDataIndex
+ */
 function displayUserInfo(userDataIndex) {
     let userObj = filteredArray[userDataIndex];
     let divModalInfoContainer = document.querySelector('div.modal-info-container');
@@ -117,7 +152,10 @@ function displayUserInfo(userDataIndex) {
         <p class="modal-text">Birthday: ${match}</p>`;
 }
 
-function addBrowsingButtons(userDataIndex) {
+/**
+ * Creates the buttons on the modal pop-up, for showing the previous or the next user
+ */
+function addBrowsingButtons() {
     let divModalButtons = document.createElement('div');
     divModalButtons.classList.add("modal-btn-container");
     divModalButtons.innerHTML =
@@ -125,7 +163,15 @@ function addBrowsingButtons(userDataIndex) {
          <button type="button" id="modal-next" class="modal-next btn">Next</button>`;
     document.querySelector('div.modal-container').appendChild(divModalButtons);
     divModalButtons.classList.add( 'zoomIn',  'animated');
+}
 
+/**
+ * Attaches the click event on the `previous` and `next` button and displays the info for the according user.
+ * If checks for the corner cases - if the 1st user is shown and the prev button pressed, it shows the last user;
+ * if the last user is shown and the next button pressed, it shows the 1st user
+ * @param {Number} userDataIndex
+ */
+function attachEventsOnModalButtons(userDataIndex) {
     $('#modal-prev').click(event => {
         if (userDataIndex == 0) {
             userDataIndex = filteredArray.length - 1;
@@ -147,7 +193,11 @@ function addBrowsingButtons(userDataIndex) {
     });
 }
 
-function addSearchTextField() {
+/**
+ * Adds a search text field and search button on the page for filtering of the displayed users.
+ * Attaches the events performed on the text field and the button - input and click
+ */
+function createSearchForm() {
     let divSearchContainer = document.querySelector('div.search-container');
 
     divSearchContainer.innerHTML += `
@@ -155,40 +205,40 @@ function addSearchTextField() {
                             <input type="search" id="search-input" class="search-input" placeholder="Search...">
                             <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
                         </form> `;
-    //because of string literal, the vanilla JS addEventListener does not trigger on elements
-    //for that reason jQuery will be used
 
-    $('.search-input').keyup(event => handleKeyUp(event));
-    $('.search-input').mouseup(event => handleMouseup(event));
-    $('.search-submit').click(event => handleClick(event));
+    document.querySelector('.search-input').addEventListener('input', handleOnSearchFieldInput);
+    document.querySelector('.search-submit').addEventListener('click', handleSearchButtonClick);
 }
 
-function handleKeyUp(event) {
-    let term = event.target.value;
-    searchTerm(term);
-}
-
-function handleClick(event) {
+/**
+ * When the user presses the magnifier icon, it triggers the filtering against the given input in the search text field
+ * @param {Event} event
+ */
+function handleSearchButtonClick(event) {
     event.preventDefault();
     let term = document.querySelector('input#search-input').value;
     searchTerm(term);
 }
 
-function handleMouseup(event){
-    setTimeout(function(){
-        let term = document.querySelector('input#search-input').value;
-        if (term === ""){
-            searchTerm(term);
-        }
-    }, 1);
+/**
+ * When the content changes in the search text field, it triggers the filtering against the given input
+ * @param {Event} event
+ */
+function handleOnSearchFieldInput(event){
+    searchTerm(event.target.value);
 }
 
+/**
+ * Filters the users with the given search term and updates the DOM with the results.
+ * Filtering is plain text and NOT case sensitive.
+ * @param {String} searchTerm
+ */
 function searchTerm(searchTerm) {
     if(searchTerm === ""){
         generateCards(userDataArray);
         return;
     }
-    searchTerm = searchTerm.toLowerCase();
+    searchTerm = searchTerm.toLowerCase().trim();
     let searchResults = [];
     userDataArray.forEach(user => {
         let userFirstName = user.name.first.toLowerCase();
